029e87d514a326a4ee45bd1441cb536a2b21cfb7
Akshay Jayaram <akshayj1814@gmail.com>
2024-06-27 14:12:23 -0700

    Add chat sheet

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,8 +1,73 @@
-import React from 'react'
+'use client'
 
-function Chat() {
+import {MessageCircle } from 'lucide-react'
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from './ui/sheet'
+import { buttonVariants } from './ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Chat = () => {
+
+    const chatCount = 0
+
   return (
-    <div>Chat</div>
+    <Sheet>
+      <SheetTrigger className='group -m-2 flex items-center p-2'>
+        <MessageCircle
+          aria-hidden='true'
+          className='h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500'
+        />
+        <span className='ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800'>
+          0
+        </span>
+      </SheetTrigger>
+      <SheetContent className='flex w-full flex-col pr-0 sm:max-w-lg'>
+        <SheetHeader className='space-y-2.5 pr-6'>
+          <SheetTitle>Chat (0)</SheetTitle>
+        </SheetHeader>
+
+        {chatCount > 0 ? (
+          <>
+            {/* Chat service */}
+          </>
+        ) : (
+          <div className='flex h-full flex-col items-center justify-center space-y-1'>
+            {/*<div
+              aria-hidden='true'
+              className='relative mb-4 h-60 w-60 text-muted-foreground'>
+              <Image
+                src='/nav/logo.svg'
+                fill
+                alt='empty shopping cart hippo'
+              />
+            </div>*/}
+            <div className='text-xl font-semibold'>
+              No ongoing chats
+            </div>
+            <SheetTrigger asChild>
+              <Link
+                href='/products'
+                className={buttonVariants({
+                  variant: 'link',
+                  size: 'sm',
+                  className:
+                    'text-sm text-muted-foreground',
+                })}>
+                Chat People
+              </Link>
+            </SheetTrigger>
+          </div>
+        )}
+
+      </SheetContent>
+    </Sheet>
   )
 }
 
